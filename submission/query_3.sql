e014c7e21201f52ac625c6c9eb988d043de1ba9d
Hamzah Arif Qadeer <qadeer.hamzah97@gmail.com>
2024-05-20 23:42:15 -0400

    Initial submission query_3.sql

---
+++ submission/query_3.sql
@@ -0,0 +1,34 @@
+-- query_3
+INSERT INTO user_devices_cumulated
+WITH
+  -- previously backfilled data
+  yesterday AS (
+    SELECT *
+      FROM user_devices_cumulated
+     WHERE date = DATE('2022-12-31')
+  ),
+  -- upcoming data to be loaded by user_id and browser_type
+  today AS (
+    SELECT
+      w.user_id,
+      d.browser_type,
+      CAST(date_trunc('day', w.event_time) AS date) AS event_date,
+      COUNT(1)
+      FROM bootcamp.web_events w
+      JOIN bootcamp.devices d ON w.device_id = d.device_id
+     WHERE date_trunc('day', w.event_time) = DATE('2023-01-01')
+     GROUP BY w.user_id, d.browser_type, date_trunc('day', event_time)
+  )
+  
+SELECT
+  COALESCE(y.user_id, t.user_id) AS user_id,
+  COALESCE(y.browser_type, t.browser_type) AS browser_type,
+  -- Append event_date if dates_active exist, else create single-item array with event_date
+  CASE
+    WHEN y.dates_active IS NOT NULL THEN ARRAY[t.event_date] || y.dates_active
+    ELSE ARRAY[t.event_date]
+  END AS dates_active,
+  DATE'2023-01-01' AS date
+FROM
+  yesterday y
+  FULL OUTER JOIN today t ON y.user_id = t.user_id AND y.browser_type = t.browser_type
