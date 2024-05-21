23f3645d8592046d249e1b77312f7a3de04599ca
Hamzah Arif Qadeer <qadeer.hamzah97@gmail.com>
2024-05-20 23:37:30 -0400

    Initial submission query_2.sql

---
+++ submission/query_2.sql
@@ -0,0 +1,11 @@
+-- query_2
+
+CREATE TABLE user_devices_cumulated (
+  user_id BIGINT,
+  browser_type VARCHAR,
+  dates_active ARRAY(date),
+  date DATE
+) WITH (
+  format = 'PARQUET',
+  partitioning = ARRAY['date']
+)
