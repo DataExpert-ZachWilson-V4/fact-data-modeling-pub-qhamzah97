ff6efac75bc1e0d0072e2a41638515dea9ecffca
Hamzah Arif Qadeer <qadeer.hamzah97@gmail.com>
2024-05-21 23:41:43 -0400

    Comments - query_6.sql

--- submission/query_6.sql
+++ submission/query_6.sql
@@ -1,11 +1,13 @@
 -- query_6
-INSERT INTO hosts_cumulated
+INSERT INTO qhamzah.hosts_cumulated
 WITH
+  -- Select previously backfilled data from the specified date
   yesterday AS (
     SELECT *
-      FROM hosts_cumulated
+      FROM qhamzah.hosts_cumulated
      WHERE date = DATE('2022-12-31')
   ),
+  -- Select new data to be loaded by host for the specified date
   today AS (
     SELECT
       host,
@@ -15,8 +17,10 @@ WITH
      GROUP BY host, CAST(date_trunc('day', event_time) AS DATE)
   )
 
+-- Combine data from 'yesterday' and 'today'
 SELECT
   COALESCE(y.host, t.host) AS host,
+  -- Append host_activity_datelist from 'today' if it exists, otherwise create a single-item array with host_activity_datelist from 'today'
   CASE
     WHEN y.host_activity_datelist IS NOT NULL THEN ARRAY[t.host_activity_datelist] || y.host_activity_datelist
     ELSE ARRAY[t.host_activity_datelist]
