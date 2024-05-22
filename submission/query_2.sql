15981bd7f9b037e580928031c6cd02722645013a
Hamzah Arif Qadeer <qadeer.hamzah97@gmail.com>
2024-05-21 23:40:28 -0400

    Update query_2.sql
    Added schema

--- submission/query_2.sql
+++ submission/query_2.sql
@@ -1,6 +1,6 @@
 -- query_2
 -- Create a new table or replace the existing table 'user_devices_cumulated'
-CREATE OR REPLACE TABLE user_devices_cumulated (
+CREATE OR REPLACE TABLE qhamzah.user_devices_cumulated (
   user_id BIGINT,
   browser_type VARCHAR,
   dates_active ARRAY(date),
