c39cef755f3b7701b79e4f9369d382e8a314c30c
Hamzah Arif Qadeer <qadeer.hamzah97@gmail.com>
2024-05-21 23:40:50 -0400

    Comments - query_4.sql

--- submission/query_4.sql
+++ submission/query_4.sql
@@ -1,10 +1,12 @@
 -- query_4
 WITH
+  -- Select data from 'user_devices_cumulated' table for the specified date
   today AS (
     SELECT *
-      FROM user_devices_cumulated
+      FROM qhamzah.user_devices_cumulated
      WHERE date = DATE('2023-01-07')
   ),
+  -- Convert the list of active dates into an integer representing the binary history of user activity
   date_list_int AS (
     SELECT 
       user_id,
@@ -14,6 +16,7 @@ WITH
         CAST(
           SUM(
             CASE
+              -- Check if the active dates contain the sequence date and calculate the binary position
               WHEN CONTAINS(dates_active, sequence_date) THEN POW(2, 30 - DATE_DIFF('day', sequence_date, date))
               ELSE 0
             END
@@ -29,6 +32,6 @@ WITH
 SELECT
   user_id,
   browser_type,
-  -- ensuring a 32-bit value
+  -- Ensure the binary history is represented as a 32-bit value by padding with leading zeros
   LPAD(history_int, 32, '0') AS history_binary
   FROM date_list_int
