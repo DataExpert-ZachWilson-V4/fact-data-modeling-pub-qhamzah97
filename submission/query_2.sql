be9a7dfefa40781d3bbb0ee04eeaa75eff3597b3
Hamzah Arif Qadeer <qadeer.hamzah97@gmail.com>
2024-05-21 23:39:00 -0400

    Comments - query_2.sql

--- submission/query_2.sql
+++ submission/query_2.sql
@@ -1,4 +1,5 @@
 -- query_2
+-- Create a new table or replace the existing table 'user_devices_cumulated'
 CREATE OR REPLACE TABLE user_devices_cumulated (
   user_id BIGINT,
   browser_type VARCHAR,
